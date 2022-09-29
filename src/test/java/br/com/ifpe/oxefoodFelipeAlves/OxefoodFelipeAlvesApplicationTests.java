7e9e0a58afae7801b605dc1b1ffb79a965352192
Leonardo da Rocha <franca.leonardorocha@gmail.com>
2022-09-29 09:13:34 -0300

    primeiros arquivos

---
+++ src/test/java/br/com/ifpe/oxefoodFelipeAlves/OxefoodFelipeAlvesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefoodFelipeAlves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OxefoodFelipeAlvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
